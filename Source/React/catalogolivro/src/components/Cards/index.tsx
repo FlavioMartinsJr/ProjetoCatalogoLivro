8c6f814ea5833bb5088a3ad6ee9f648251d19dc2
Flavio Martins Jr <flaviojunior1070@gmail.com>
2024-02-20 18:21:51 -0300

    up

---
+++ Source/React/catalogolivro/src/components/Cards/index.tsx
@@ -0,0 +1,12 @@
+import './cards.scss'
+
+
+export function Cads() {
+
+    return(
+        <div className="principal">
+        </div>
+    );
+}
+
+
--- Source/React/catalogolivro/src/pages/Home/index.tsx
+++ Source/React/catalogolivro/src/pages/Home/index.tsx
@@ -1,10 +1,26 @@
+import { faGrip} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { Cads } from "../../components/Cards/index";
 import "./home.scss";
 
 export function Home(){
 
     return(
-        <section className="">
-            asdasf
+        <section className="fundo">
+            <div className="fundo-titulo">
+                <div className="fundo-titulo-left">
+                    <h5>Home</h5>
+                    <h2>Catálogo de Livros</h2>
+                </div>
+                <div className="fundo-titulo-right">
+                    <p><FontAwesomeIcon icon={faGrip}/>  12 resultados</p>
+                </div>
+            </div>
+            
+            
+
+            <Cads/>
         </section>
     )
 }
