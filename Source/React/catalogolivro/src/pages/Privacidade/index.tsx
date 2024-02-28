02671deb6406b18004756d91bc70cfe6c8c7ade0
Flavio Martins Jr <flaviojunior1070@gmail.com>
2024-02-28 15:27:01 -0300

    up

--- Source/React/catalogolivro/src/pages/Privacidade/index.tsx
+++ Source/React/catalogolivro/src/pages/Privacidade/index.tsx
@@ -11,8 +11,11 @@ export function Privacidade(){
     return(
         <section className="fundo-privacidade">
 
-            {Tittle("Home / Privacidade ","Politica e Privacidade" ,"",false)}
-            
+            {Tittle("Home / Privacidade ","" ,"",false)}
+            <div className="container privacidade">
+                <h3>Politica e Privacidade</h3>
+                <p>Nossa política de privacidade visa proteger os dados dos usuários. Coletamos informações pessoais apenas quando fornecidas voluntariamente. Esses dados são utilizados exclusivamente para os fins informados no momento da coleta, como processamento de pedidos e comunicações sobre promoções. Adotamos medidas de segurança para proteger as informações contra acesso não autorizado. Não compartilhamos informações pessoais com terceiros sem consentimento, exceto por obrigações legais. Utilizamos cookies para melhorar a experiência do usuário, os quais podem ser desativados nas configurações do navegador. Reservamo-nos o direito de atualizar esta política periodicamente e incentivamos os usuários a revisá-la regularmente.</p>
+            </div>
 
         </section>
     )
--- Source/React/catalogolivro/src/pages/Termos/index.tsx
+++ Source/React/catalogolivro/src/pages/Termos/index.tsx
@@ -11,8 +11,11 @@ export function Termos(){
     return(
         <section className="fundo-termos">
 
-            {Tittle("Home / Termos de Uso ","Termos de Uso" ,"",false)}
-            
+            {Tittle("Home / Termos de Uso ","" ,"",false)}
+            <div className="container termos">
+                <h3>Termos de Uso</h3>
+                <p>Nossos Termos de Uso estabelecem as regras e condições para o uso dos nossos serviços. Ao acessar o nosso site, você concorda em cumprir estes termos. O conteúdo disponível no site é protegido por direitos autorais e propriedade intelectual. Você concorda em usar o site apenas para fins legais e não infringir os direitos de terceiros. Não é permitido utilizar o site para atividades ilegais, prejudiciais ou que possam violar a privacidade de outros usuários. Reservamos o direito de modificar ou encerrar o serviço a qualquer momento, sem aviso prévio. O conteúdo gerado pelos usuários pode ser removido se violar estes termos. Não nos responsabilizamos por danos diretos, indiretos, incidentais ou consequenciais decorrentes do uso do site. Ao usar o site, você concorda em isentar-nos de qualquer responsabilidade. Estes termos estão sujeitos a alterações sem aviso prévio. Recomendamos que você os revise regularmente.</p>
+            </div>
 
         </section>
     )
