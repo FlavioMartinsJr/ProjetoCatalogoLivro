3adc7155c586c469dccdc765378562f6e443e595
Flavio Martins Jr <flaviojunior1070@gmail.com>
2024-02-28 18:51:14 -0300

    up

--- Source/React/catalogolivro/src/pages/Sobre/index.tsx
+++ Source/React/catalogolivro/src/pages/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import { Tittle } from "../../components/Tittle";
 import { ScrollToTop } from "../../hooks/Functions/GoToTop"
+import { Link } from "react-router-dom"
 import "./sobre.scss";
 
 
@@ -11,9 +12,14 @@ export function Sobre(){
     return(
         <section className="fundo-sobre">
 
-            {Tittle("Home / Sobre ","Ver Sobre" ,"",false)}
+            {Tittle("Home / Sobre ","" ,"",false)}
+            <div className="container sobre">
+                <h3>Ver Sobre</h3>
+                <p>O catalogo de Livros serve apenas para fins academicos de uso para promover a apredizagem em praticas de desenvolvimento de sistemas. Olhe tambem nossas <Link className="link" to={"/Privacidade"}>Politica de Privacidade</Link> e <Link className="link" to={"/TermosDeUso"}>Termos de Uso</Link></p>
+                <p>(Texto e informações fictícias)</p>
+            </div>
             
-         
+            
 
         </section>
     )
